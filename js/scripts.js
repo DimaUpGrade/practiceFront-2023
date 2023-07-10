4779467b7268b280fae5240fa6c03ad3b7a5bf38
DimaUpGrade <dima2002bord@yandex.ru>
2023-07-10 15:03:40 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(".count-d-n-p-1").on("click", function() {
+	$(".count-d-n-p-1").removeClass("active");
+	$(event.target).addClass("active");
+})
+
+$(".count-d-n-p-2").on("click", function() {
+	$(".count-d-n-p-2").removeClass("active");
+	$(event.target).addClass("active");
+})
+
